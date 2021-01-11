ea1b89d7497d012a7bc3092f0f1b6f865946fc28
Zach Mead <zachary.n.mead@gmail.com>
2021-01-10 19:46:07 -0600

    starting functions

--- scripts/client.js
+++ scripts/client.js
@@ -2,8 +2,17 @@ console.log ('js');
 $(document).ready(readyNow);
 
 function readyNow() {
-
+    $(#submitButton).on('click', addEmployee);
+    $(#submitButton).on('click', compileSalary);
 }
 
 let employees = [];
-let 
+let index = 0; 
+let monthlyTotal= "$" + 0; 
+
+
+
+
+
+// $('table').on('click', deleteEmployee);
+// $('table').on('click', compileSalary);
